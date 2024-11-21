da8a109f6b3bdec863323fe17addd696502f5fc3
KiraCorbett <kiracorbett00@gmail.com>
2024-11-20 22:31:51 -0800

    landing about page

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,12 +1,59 @@
 import Header from "./components/Header";
 import ProjectCard from "./components/ProjectCard";
+import kira from "./assets/kira.jpeg";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
-      <section id="projects">
+      <section id="info">
+        <div className="left-col">
+          <h1>Also known as "The Real Kira AI."</h1>
+          <h3>
+            Kira Corbett is a software engineer, curriculum developer, and
+            instructor. As a software engineer, she has focused on robotics,
+            simulation, drone data services, and full-stack development at
+            Oregon State University, Autodesk, and Aerial Data Labs. Kira is a
+            Founding Engineer for 2 startups - one of which integrates 3D
+            photogrammetry work. Today, Kira continues to fuse the exploration
+            in AI and human-computer interactions within education sectors
+            through development.
+          </h3>
+          <a
+            href="https://github.com/TheRealKiraAI"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/kira-corbett/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://x.com/TheRealKiraAI"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Social
+          </a>
+          <a
+            href="https://ir.library.oregonstate.edu/concern/honors_college_theses/3t945z87r"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Publication
+          </a>
+        </div>
+        <div className="right-col">
+          <img src={kira} alt="Kira Corbett Profile Image" />
+        </div>
+      </section>
+      {/* <section id="projects">
         <ProjectCard
           title="Project 1: Sloth Clothing Store"
           description="A fun online store for sloth-themed clothing."
@@ -22,7 +69,7 @@ function App() {
           description="A real-time music visualizer powered by AI."
           link="https://your-project-link.com"
         />
-      </section>
+      </section> */}
     </>
   );
 }
--- website/src/components/Header.jsx
+++ website/src/components/Header.jsx
@@ -1,11 +1,16 @@
 import { Image, React } from "react";
-import crow from "../assets/crow.jpg";
+import crow from "../assets/no-crow.png";
+import dark from "../assets/dark.png";
 
 const Header = () => {
   return (
-    <header>
+    <header className="header-div">
+      {/* <img src={crow} alt="Portfolio Header Image" className="header-image" /> */}
       <h1>Kira Corbett</h1>
-      <img src={crow} alt="Portfolio Header Image" className="header-image" />
+      <h3>Developer Educator || Software Engineer || Startup Enthuisast</h3>
+      {/* <h3>
+        Taking <span style={{ color: "purple" }}>Ideas</span> to Flight.
+      </h3> */}
     </header>
   );
 };
